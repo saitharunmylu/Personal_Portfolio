d42026ea564ba7f0b26930b62fcf117ba33f7a64
saitharunmylu <saitharun.mylu@gmail.com>
2026-03-01 17:51:14 +0530

    Create sheet.js

---
+++ sheet.js
@@ -0,0 +1,10 @@
+<script>
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+</script>
+    });
+});
